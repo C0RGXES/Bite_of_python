7915a9e601ab899d15eaf141f9087047d211c7da
iliaHomeDesctop <ilia97borovkov@yandex.ru>
2021-03-17 20:25:32 +0300

    add doc_string.py and import_module.py *modify str_len.py file, add function

--- str_len.py
+++ str_len.py
@@ -1,10 +1,18 @@
-print("HI! Do you want to know the length of a string?\n")
 
-while True:
-    string = input("""Print some string here!\nPS if you want to exit print 'exit'\n""")
-    if string == "exit":
-        break
-    if len(string) < 3:
-        print("ERROR! Your string is too weak. Length must be more than 3")
-        continue
-    print("String length", len(string))
+def length_string():
+    print("HI! Do you want to know the length of a string?\n")
+
+    while True:
+        string = input("""Print some string here!\nPS if you want to exit print 'exit'\n""")
+        if string == "exit":
+            break
+        if len(string) < 3:
+            print("ERROR! Your string is too weak. Length must be more than 3")
+            continue
+        print("String length", len(string))
+
+
+if __name__ == "__main__":
+    length_string()
+else:
+    print('hey! I''m imported')
