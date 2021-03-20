5b87eb3852921f1d42c9088b4257d0b55e722ba1
iliaHomeDesctop <ilia97borovkov@yandex.ru>
2021-03-20 19:50:06 +0300

    job is done

--- problem_solving.py
+++ problem_solving.py
@@ -2,16 +2,36 @@ import os
 import time
 
 # 1. Файлы и каталоги, которые необходимо скопировать, собираются в список.
-source = ['E:\\Books', '"E:\\Cooking books"']
+source = ['D:\\test', 'D:\\test\\test2']
 # Заметьте, что для имён, содержащих пробелы, необходимо использовать
 # двойные кавычки внутри строки.
 
 # 2. Резервные копии должны храниться в основном каталоге резерва.
-target_dir = 'E:\\Backup'  # Подставьте тот путь, который вы будете использовать.
+target_dir = 'D:\\Backup'  # Подставьте тот путь, который вы будете использовать.
+
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+    print('Каталог для архива создан')
 
 # 3. Файлы помещаются в zip-архив.
-# 4. Именем для zip-архива служит текущая дата и время.
-target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+
+# 4. Текущая дата служит именем подкаталога в основном каталоге
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+# Текущее время служит именем zip-архива
+now = time.strftime('%H%M%S')
+
+# Запрашиваем комментарий пользователя для имени файла
+comment = input('Введите комментарий --> ')
+if len(comment) == 0:  # проверяем, введён ли комментарий
+    target = today + os.sep + now + '.zip'
+else:
+    target = today + os.sep + now + '_' + \
+        comment.replace(' ', '_') + '.zip'
+
+# Создаём каталог, если его ещё нет
+if not os.path.exists(today):
+    os.mkdir(today)  # создание каталога
+    print('Каталог успешно создан', today)
 
 # 5. Используем команду "zip" для помещения файлов в zip-архив
 zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
